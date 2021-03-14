93714150625214e882810cf98a6c5341724ef784
Filip Davidovic <filip.davidovic@protonmail.com>
2021-03-15 00:55:12 +0100

    made transitions to normal map fluent

--- js/main.js
+++ js/main.js
@@ -1,39 +1,6 @@
 // Constants
 const electionYears = ['2012', '2016', '2020'];
 
-const presidentialCandidates = {
-    '2012': {
-        'democrats': {
-            'name': 'Barack Obama',
-            'profile': 'https://images-na.ssl-images-amazon.com/images/I/51NuSfifT-L._AC_.jpg'
-        },
-        'republicans': {
-            'name': 'Mitt Romney',
-            'profile': 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Mitt_Romney_Circle.png'
-        }
-    },
-    '2016': {
-        'democrats': {
-            'name': 'Hillary Clinton',
-            'profile': 'https://pngimg.com/uploads/hillary_clinton/hillary_clinton_PNG26.png'
-        },
-        'republicans': {
-            'name': 'Donald Trump',
-            'profile': 'https://paaia.org/wp-content/uploads/2020/02/Donald-Trump-circle-1024x1024.png'
-        }
-    },
-    '2020': {
-        'democrats': {
-            'name': 'Joe Biden',
-            'profile': 'https://upload.wikimedia.org/wikipedia/commons/2/26/Biden_Circle.png'
-        },
-        'republicans': {
-            'name': 'Donald Trump',
-            'profile': 'https://paaia.org/wp-content/uploads/2020/02/Donald-Trump-circle-1024x1024.png'
-        }
-    }
-}
-
 const mapColors = {
     ELECTION_RESULTS: 'election-results',
 };
@@ -100,29 +67,6 @@ $('.content-changer').on('click', function() {
     setContent();
 });
 
-/** set up static HTML messages to be displayed here **/
-
-var template_electoralCollege = '<p class="fade-in" style="padding:20px"> The United States Electoral College is the group of presidential electors required by the Constitution to form every four years for the sole purpose of electing the president and vice president. Each state appoints electors according to its legislature, equal in number to its congressional delegation (senators and representatives). '+
-    '<br> <br> Hover over each state and see details such as the number of electoral votes or the population. </p>';
-
-var template_longTerm_socialClass = '<p class="fade-in" style="padding:20px">If we sort Americans by formal educational levels, the partisan gaps are not as great. But if we focus on income, class makes more of a difference in how citizens vote now than it did fifty years ago – even with all the racial changes America has experienced. <br> <br> At a time when Americans are becoming steadily more educated, incomes are not increasing much for most people – and income gaps are growing. High earners look to Republicans, while Democrats are, more than ever, the party lower-income Americans want to represent their interests.\n' +
-    '\n  <br> <h6 class="fade-in" style="font-size:10px; padding:20px;"> sources: https://scholars.org/contribution/does-class-matter-when-americans-vote <br></p>';
-
-var template_longTerm_race = '<p class="fade-in" style="padding:20px">The demographic composition of an area does not tell the whole story. Patterns in voter registration and voter turnout vary widely by race and ethnicity, with White adults historically more likely to be registered to vote and to turn out to vote than other racial and ethnic groups. Additionally, every presidential election brings its own unique set of circumstances, from the personal characteristics of the candidates, to the economy, to historic events such as a global pandemic. Still, understanding the changing racial and ethnic composition in key states helps to provide clues for how political winds may shift over time.\n' +
-    '\n' +
-    ' <br> <br> Black, Hispanic and Asian registered voters historically lean Democratic\n' +
-    'The ways in which these demographic shifts might shape electoral outcomes are closely linked to the distinct partisan preferences of different racial and ethnic groups. Pew Research Center survey data spanning more than two decades shows that the Democratic Party maintains a wide and long-standing advantage among Black, Hispanic and Asian American registered voters. Among White voters, the partisan balance has been generally stable over the past decade, with the Republican Party holding a slight advantage.\n' +
-    '\n <br> <h6 class="fade-in" style="font-size:10px; padding:20px;"> sources: <br>  https://www.pewresearch.org/2020/09/23/the-changing-racial-and-ethnic-composition-of-the-u-s-electorate/ </p>';
-
-var template_longTerm_age = '<p class="fade-in" style="padding:20px"> They say elections are decided by those who show up to vote.\n' +
-    'Age is one of the strongest long term factors when it comes to election results. It appears that the young vote less than the old and that turnout climbs with each additional year of life, is one of the most robust findings in political science. <br> <br> There is a link between age and voting behaviour. As people age they are more likely to be at the top of their earnings so they are more likely to favour traditional Conservative policies such as lower taxation on higher earners, i.e the Republican party. In a 2017 general election in UK, 61% of voters nationally who were aged over 65-years old voted Conservative.\n' +
-    '\n' +
-    'Younger voters may be more concerned with issues such as greater support for education or youth unemployment. Those under 35-years of age tend not to vote for the Conservatives.\n' +
-    '\n <br> <h6 class="fade-in" style="font-size:10px; padding:20px;"> sources: <br> https://www.bbc.co.uk/bitesize/guides/zd9bd6f/revision/5 <br> https://medium.com/@PollsAndVotes/age-and-voter-turnout-52962b0884ef </h6> </p>';
-
-
-/** end of constants section **/
-
 function setContent() {
     let year = $('select option:selected').val();
     let factorType = $('#factor-selector label.active').find('input');
@@ -136,35 +80,32 @@ function setContent() {
 
         $storyline.html(html);
 
-        initMap();
+        resetMap();
         updateMapColors(mapColors.ELECTION_RESULTS);
     } else if (factorType.val() === 'electoral-college') {
-        $storyline.html(template_electoralCollege);
+        $storyline.loadTemplate('templates/storyline/electoral_college.html');
 
         updateMap();
     } else if (factorType.val() === 'long-term') {
         let factor = $('#long-term-row label.active').find('input');
-        let html;
 
         if (factor.length === 0) {
-            html = '<h2 class="text-center">Select a long term factor from <span class="green">green</span> buttons.</h2>';
+            $storyline.html('<h2 class="text-center">Select a long term factor from <span class="green">green</span> buttons.</h2>');
         } else if (factor.val() === 'social-class') {
-            html = template_longTerm_socialClass;
+            $storyline.loadTemplate('templates/storyline/long_term_social_class.html');
 
             // TODO: Update the map
         } else if (factor.val() === 'race') {
-            html = template_longTerm_race;
+            $storyline.loadTemplate('templates/storyline/long_term_race.html');
 
             // TODO: Update the map
         } else if (factor.val() === 'age') {
-            html = template_longTerm_age;
+            $storyline.loadTemplate('templates/storyline/long_term_age.html');
 
             // TODO: Update the map
         } else {
             throw Error('Unexpected value for long term factor: ' + factor.val());
         }
-
-        $storyline.html(html);
     } else if (factorType.val() === 'short-term') {
         let factor = $('#short-term-row label.active').find('input');
         let html;
@@ -210,6 +151,7 @@ let tooltip = $('#tooltip');
 let topology,
     geometries,
     dataByState,
+    presidentialCandidates,
     proj = d3.geo.albersUsa(),
     carto = d3.cartogram()
         .projection(proj)
@@ -237,8 +179,8 @@ let topology,
             };
         });
 
-d3.json('data/us.topojson', function(topo) {
-    d3.json('data/elections.json', function(data) {
+d3.json('data/usa.topojson', function(topo) {
+    d3.json('data/by_state.json', function(data) {
         dataByState = data;
 
         topology = topo;
@@ -246,7 +188,11 @@ d3.json('data/us.topojson', function(topo) {
             // Use only polygons for which we have the data
             .filter((poly) => poly.id in dataByState);
 
-        initMap();
+        d3.json('data/presidential_candidates.json', function(candidateData) {
+            presidentialCandidates = candidateData;
+
+            initMap();
+        });
     });
 });
 
@@ -264,6 +210,9 @@ function updateElectionResultsIndicator() {
             $electionResultsBar.removeClass(`e-${electionYear}`);
         }
     }
+
+    $('#election-results-vote-counter-democrats').text(presidentialCandidates[year]['democrats']['votes']);
+    $('#election-results-vote-counter-republicans').text(presidentialCandidates[year]['republicans']['votes']);
 }
 
 /**
@@ -322,6 +271,8 @@ function updateMapColors() {
  * dropdowns and radio buttons.
  */
 function updateMap() {
+    // d3.polygonArea(d.geometry.coordinates[0])
+
     let values = states.data()
         .map((d) => d.properties.electoralVotes)
         .filter((n) => !isNaN(n))
@@ -343,6 +294,17 @@ function updateMap() {
         .attr('d', carto.path);
 }
 
+function resetMap() {
+    let features = carto.features(topology, geometries);
+    let path = d3.geo.path().projection(proj);
+
+    states.data(features);
+    states.transition()
+        .duration(750)
+        .ease('linear')
+        .attr('d', path);
+}
+
 /**
  * Show the tooltip for a state.
  *
@@ -358,7 +320,7 @@ function showTooltip(d, id, data) {
         tooltip
             .css('left', d3.event.clientX + 15)
             .css('top', d3.event.clientY + 15)
-            .loadTemplate('templates/electoral_college.tooltip.html', {
+            .loadTemplate('templates/tooltip/electoral_college.html', {
 
                 // set those according to what you want to see with the tooltip.
                 // let us first implement the electoral_college case
@@ -373,7 +335,7 @@ function showTooltip(d, id, data) {
         tooltip
             .css('left', d3.event.clientX + 15)
             .css('top', d3.event.clientY + 15)
-            .loadTemplate('templates/short-term.tooltip.html', {
+            .loadTemplate('templates/tooltip/short_term.html', {
 
                 stateName: d.properties.name,
 
@@ -385,7 +347,7 @@ function showTooltip(d, id, data) {
         tooltip
             .css('left', d3.event.clientX + 15)
             .css('top', d3.event.clientY + 15)
-            .loadTemplate('templates/long-term.tooltip.html', {
+            .loadTemplate('templates/tooltip/long_term.html', {
 
                 stateName: d.properties.name,
 
@@ -397,7 +359,7 @@ function showTooltip(d, id, data) {
         tooltip
             .css('left', d3.event.clientX + 15)
             .css('top', d3.event.clientY + 15)
-            .loadTemplate('templates/context_unset.tooltip.html', {
+            .loadTemplate('templates/tooltip/context_unset.html', {
 
                 stateName: d.properties.name,
 
